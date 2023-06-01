17c19fc808249db921aa9b2fa583bb44f42e2ebf
sleepycode03 <kimtfl03@naver.com>
2023-06-01 23:10:18 +0900

    글 생성 기능과 리스트 조회 기능 추가

--- app.js
+++ app.js
@@ -2,54 +2,57 @@ const express = require('express');
 const morgan = require("morgan");
 const path = require("path");
 const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const db_controler = function(query_text) {
-    //db 연결-------------------------------------------------------
-    let db = new sqlite3.Database('database.db', sqlite3.OPEN_READWRITE, (err) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log('connected database');
-    });
-    //--------------------------------------------------------------
-
-    let sql = query_text;
-
-    db.all(sql, [], (err, rows) => {
+//db 연결-------------------------------------------------------
+let db = new sqlite3.Database('database.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
-        throw err;
+        return console.error(err.message);
     }
-    rows.forEach((row) => {
-        console.log(row);
-    });
-    });
+    console.log('connected database');
+});
+//--------------------------------------------------------------
 
-    //db 해제-------------------------------------------------------
-    db.close((err) => {
-        if(err) {
-            return console.error(err.message);
-        }
-        console.log("close database");
-    });
-    //--------------------------------------------------------------
-}
+
+//db 해제-------------------------------------------------------
+// db.close((err) => {
+//     if(err) {
+//         return console.error(err.message);
+//     }
+//     console.log("close database");
+// });
+//--------------------------------------------------------------
+
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.use(morgan('common'));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //URL별 처리---------------------------------------------------
 app.get('/', (req, res) => {
-    db_controler(`SELECT * FROM test_table`);
-    return res.render('index', {title:"제목"});
+    db.all(`SELECT * FROM post`, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+            console.log(rows);
+            res.render('index', {post_list:rows});
+    });
 });
 
 app.get('/create', (req, res) => {
     return res.render('post_create', {title:"제목"});
+});
+
+app.post('/create', (req, res) => {
+    console.log("HIHI");
+    db.run(`INSERT INTO post (title, detail) VALUES ('${req.body.title}', '${req.body.detail}');`);
+    return res.redirect('/');
 });
 
 app.get('/delete', (req, res) => {
