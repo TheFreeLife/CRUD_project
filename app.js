97c14b51335f6d5b7b47d146206b523b10ea8457
sleepycode03 <kimtfl03@naver.com>
2023-06-01 21:21:09 +0900

    추가 버튼이랑 생성 페이지 연결

--- app.js
+++ app.js
@@ -5,27 +5,36 @@ const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 
-//db 연결-------------------------------------------------------
-let db = new sqlite3.Database('database.db', sqlite3.OPEN_READWRITE, (err) => {
+const db_controler = function(query_text) {
+    //db 연결-------------------------------------------------------
+    let db = new sqlite3.Database('database.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('connected database');
+    });
+    //--------------------------------------------------------------
+
+    let sql = query_text;
+
+    db.all(sql, [], (err, rows) => {
     if (err) {
-        return console.error(err.message);
+        throw err;
     }
-    console.log('connected database');
-});
-//--------------------------------------------------------------
+    rows.forEach((row) => {
+        console.log(row);
+    });
+    });
 
-
-db.run("INSERT INTO test_table (name) VALUES ('JOHN');");
-
-
-//db 해제-------------------------------------------------------
-db.close((err) => {
-    if(err) {
-        return console.error(err.message);
-    }
-    console.log("close database");
-});
-//--------------------------------------------------------------
+    //db 해제-------------------------------------------------------
+    db.close((err) => {
+        if(err) {
+            return console.error(err.message);
+        }
+        console.log("close database");
+    });
+    //--------------------------------------------------------------
+}
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -35,11 +44,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 //URL별 처리---------------------------------------------------
 app.get('/', (req, res) => {
+    db_controler(`SELECT * FROM test_table`);
     return res.render('index', {title:"제목"});
 });
 
 app.get('/create', (req, res) => {
-    return res.render('index', {title:"제목"});
+    return res.render('post_create', {title:"제목"});
 });
 
 app.get('/delete', (req, res) => {
